1c4f99f6d887ee77295b07cecfec1a189f951ea8
Theo <ojw1912@gmail.com>
2024-12-23 14:08:40 +0900

    241223 12:50 게시물 상세조회 페이지 구현

--- src/main/java/com/spring/mvcproject/board/api/BoardApiController.java
+++ src/main/java/com/spring/mvcproject/board/api/BoardApiController.java
@@ -1,6 +1,7 @@
 package com.spring.mvcproject.board.api;
 
 import com.spring.mvcproject.board.dto.request.BoardSaveDto;
+import com.spring.mvcproject.board.dto.response.BoardDetailResponse;
 import com.spring.mvcproject.board.dto.response.BoardListDto;
 import com.spring.mvcproject.board.entity.Board;
 import jakarta.validation.Valid;
@@ -9,11 +10,13 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-import static java.util.Comparator.*;
-import static java.util.stream.Collectors.*;
+import static java.util.Comparator.comparing;
+import static java.util.stream.Collectors.toList;
 
 @RestController
 @RequestMapping("/api/v1/boards")
@@ -88,4 +91,24 @@ public class BoardApiController {
         return ResponseEntity.ok().body("게시물 등록 성공! - "+ board);
 
     }
+
+    // 게시물 상세조회 요청처리
+    @GetMapping("/{id}")
+    public ResponseEntity<?> detail(@PathVariable Long id) {
+        // 특정 게시물 찾아오기
+        Board foundBoard = boardStore.get(id);
+
+        // 게시물이 없을 경우
+        if (foundBoard == null) {
+            return ResponseEntity
+                    .badRequest() // 400
+                    .body(id + "번 게시물은 존재하지 않습니다.");
+        }
+
+        // 게시물 원본데이터를 클라이언트 스펙에 맞게 변환
+        return ResponseEntity
+                .ok()
+                .body(BoardDetailResponse.from(foundBoard));
+    }
+
 }
