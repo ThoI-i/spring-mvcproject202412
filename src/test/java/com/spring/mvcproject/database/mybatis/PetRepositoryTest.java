61ffceb37478b713ce3ea464164c07362abbb85f
Theo <ojw1912@gmail.com>
2024-12-27 14:17:02 +0900

    241227 14:15 MyBatis 기초 - CRUD BASIC

--- src/test/java/com/spring/mvcproject/database/mybatis/PetRepositoryTest.java
+++ src/test/java/com/spring/mvcproject/database/mybatis/PetRepositoryTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 @SpringBootTest
 class PetRepositoryTest {
 
@@ -22,4 +24,32 @@ class PetRepositoryTest {
         petRepository.save(pet);
     }
 
+    @Test
+    void findByIdTest() {
+        Pet foundPet = petRepository.findById(1L);
+
+        System.out.println("\n\nfoundPet = " + foundPet);
+    }
+
+    @Test
+    void findAllTest() {
+        List<Pet> petList = petRepository.findAll();
+
+        System.out.println("\n\n =================== List of pet ===================");
+        petList.forEach(System.out::println);
+    }
+
+    @Test
+    void deleteByIdTest() {
+        petRepository.deleteById(3L);
+
+        Pet pet = petRepository.findById(3L);
+        System.out.println("pet = " + pet);
+    }
+
+    @Test
+    void countTest() {
+        int count = petRepository.petCount();
+        System.out.println("count = " + count);
+    }
 }
